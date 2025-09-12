b712d6731a38780dd560a33e729b61cad26df319
William Aaron Cheung <git@troublor.xyz>
2025-09-12 23:25:47 +0800

    fix: temporary disable increasing gas cost per-byte

--- crates/mega-evm/src/constants.rs
+++ crates/mega-evm/src/constants.rs
@@ -32,16 +32,16 @@ pub mod mini_rex {
     /// on top of the `NEW_ACCOUNT_GAS`.
     pub const CREATE_GAS: u64 = 2_000_000;
     /// The additional gas cost for `CODEDEPOSIT` for the `MINI_REX` spec, i.e., gas cost per byte
-    /// for code deposit during contract creation. `2_000_000 / 32 = 62500`. This is charged on
+    /// for code deposit during contract creation. This is charged on
     /// top of the `CODEDEPOSIT` gas cost of standard EVM.
-    pub const CODEDEPOSIT_ADDITIONAL_GAS: u64 = 62500 - super::equivalence::CODEDEPOSIT;
+    pub const CODEDEPOSIT_ADDITIONAL_GAS: u64 = 0;
     /// The gas cost for `LOGDATA` for the `MINI_REX` spec, i.e., gas cost per byte for log data.
-    pub const LOG_DATA_GAS: u64 = 800;
+    pub const LOG_DATA_GAS: u64 = 8;
     /// The gas cost for `LOGTOPIC` for the `MINI_REX` spec, i.e., gas cost per topic for log.
-    pub const LOG_TOPIC_GAS: u64 = 37500;
+    pub const LOG_TOPIC_GAS: u64 = 375;
     /// The additional gas cost for `CALLDATA` for the `MINI_REX` spec, i.e., gas cost per byte for
     /// call data. This is charged on top of the calldata cost of standard EVM.
-    pub const CALLDATA_ADDITIONAL_GAS: u64 = 400;
+    pub const CALLDATA_ADDITIONAL_GAS: u64 = 0;
 
     /// The maximum amount of data allowed to generate from a block for the `MINI_REX` spec.
     pub const BLOCK_DATA_LIMIT: u64 = 12 * 1024 * 1024 + 512 * 1024; // 12.5 MB
