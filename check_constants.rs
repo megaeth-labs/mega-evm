2df6a88bb4a23a05dd58c6559349e84223826706
William Aaron Cheung <git@troublor.xyz>
2025-11-20 16:39:11 +0800

    chore: remove garbage files

--- check_constants.rs
+++
@@ -1,8 +0,0 @@
-use revm::interpreter::gas;
-
-fn main() {
-    println!("LOGDATA: {}", gas::LOGDATA);
-    println!("LOGTOPIC: {}", gas::LOGTOPIC);
-    println!("STANDARD_TOKEN_COST: {}", gas::STANDARD_TOKEN_COST);
-    println!("TOTAL_COST_FLOOR_PER_TOKEN: {}", gas::TOTAL_COST_FLOOR_PER_TOKEN);
-}
